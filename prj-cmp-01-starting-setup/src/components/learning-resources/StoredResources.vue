331a306e3f9f6a3da2fe09a3fb055de9b46f56c2
Sayyed Abdul Latif <sayyedlatif1994@gmail.com>
2023-07-20 16:29:19 +0800

    Completed
    Added form with create new resource functionality, a dialog to let user know a field in the form is invalid, and functionality to delete a resource

--- prj-cmp-01-starting-setup/src/components/learning-resources/StoredResources.vue
+++ prj-cmp-01-starting-setup/src/components/learning-resources/StoredResources.vue
@@ -2,6 +2,7 @@
     <ul>
         <learning-resource v-for="resource in resources" 
         :key="resource.id" 
+        :id="resource.id"
         :title="resource.title"
         :description="resource.description"
         :link="resource.link"></learning-resource>
@@ -15,7 +16,7 @@ export default {
     components: {
         LearningResource
     },
-    props: ['resources']
+    inject: ['resources']
 }
 </script>
 
